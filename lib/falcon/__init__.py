c0c6fd6e14ee71ac787a4de327ba8d7a612bae4e
Max <bolgov1458@yandex.ru>
2024-02-05 08:17:34 +0300

    Update __init__.py

--- lib/falcon/__init__.py
+++ lib/falcon/__init__.py
@@ -5,4 +5,5 @@ from .falcon_model import FalconModel, forward_falcon_model
 from .attention import Attention, forward_attention
 from .layer_norm import LayerNorm, forward_layer_norm
 from .decoder import Decoder, forward_decoder
+from .decoder_block import DecoderBlock
 from .rotary_embedding import RotaryValues, get_rotary_values_at_position, make_rotary_values
