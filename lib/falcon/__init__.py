6f112a29da2d549ff9735ca36c1677585f2ee3d5
Max <bolgov1458@yandex.ru>
2024-02-05 07:49:28 +0300

    Update __init__.py

--- lib/falcon/__init__.py
+++ lib/falcon/__init__.py
@@ -1,5 +1,5 @@
-from .ModelConfig import ModelConfig, model_config_dummy, model_config_llama1_7B, model_config_llama2_13B, model_config_llama2_70B, model_config_llama2_7B
+from .ModelConfig import ModelConfig, model_config_falcon_7B
 from .kv_cache import KVCache, shift_left_kv_cache
-from .llama import Llama, check_llama, forward_llama, init_llama
-from .llama_model import LlamaModel, check_llama_model, forward_llama_model, init_llama_model
+from .falcon import Falcon, forward_falcon
+from .falcon_model import FalconModel, forward_falcon_model
 from .rotary_embedding import RotaryValues, get_rotary_values_at_position, make_rotary_values
