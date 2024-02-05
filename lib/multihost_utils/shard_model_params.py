61a62b2b6597afe83cf1c0bb1662852c45918581
Max <bolgov1458@yandex.ru>
2024-02-05 07:29:48 +0300

    Update shard_model_params.py

--- lib/multihost_utils/shard_model_params.py
+++ lib/multihost_utils/shard_model_params.py
@@ -1,25 +1,23 @@
 import jax
 
-from ..llama import Llama, LlamaModel
-from ..llama.attention import Attention
-from ..llama.decoder import Decoder
+from ..falcon import Falcon, FalconModel
+from ..falcon.attention import Attention
+from ..falcon.decoder import Decoder
 from .shard_array import shard_array
 
-sharding_mp = Llama(
-    model=LlamaModel(
+sharding_mp = Falcon(
+    model=FalconModel(
         embedding=...,
-        decoder=Decoder(
-            input_norm=...,
-            attention=Attention(q_proj=3, k_proj=2, v_proj=2, out_proj=2),
-            post_attn_norm=...,
-            gate_proj=2,
-            up_proj=2,
-            down_proj=1,
+        decoder=DecoderBlock(
+            layer_norm=LayerNorm(input_norm=..., input_norm_bias=...),
+            attention=Attention(query_key_value=2, dense=2),
+            dense_h_to_4h=2,
+            dense_4h_to_h=1,
         ),
-        norm=...,
+        norm=LayerNorm(input_norm=..., input_norm_bias=...),
     ),
     lm_head=...,
 )
 
-def shard_model_params(params: Llama) -> Llama:
+def shard_model_params(params: Falcon) -> Falcon:
     return jax.tree_map(shard_array, params, sharding_mp)
