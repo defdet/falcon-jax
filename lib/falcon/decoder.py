519bf77770071463a2baafbf267d35592325fe10
Max <bolgov1458@yandex.ru>
2024-02-05 07:58:08 +0300

    Update decoder.py

--- lib/falcon/decoder.py
+++ lib/falcon/decoder.py
@@ -7,19 +7,9 @@ import jax.random as rand
 from ..rand_utils import split_key_nullable
 from ..tree_utils import stack_leaves
 from .ModelConfig import ModelConfig
-from .decoder_block import DecoderBlock, DecoderBlock as Decoder, check_decoder_block, forward_decoder_block, init_decoder_block
+from .decoder_block import DecoderBlock, DecoderBlock as Decoder, forward_decoder_block
 from .kv_cache import KVCache
 from .rotary_embedding import RotaryValues
-
-def check_decoder(params: Decoder, *, model_config: ModelConfig) -> None:
-    def inner(state, input_):
-        assert isinstance(input_, DecoderBlock)
-        check_decoder_block(input_, model_config=model_config)
-        return None, None
-    jax.lax.scan(inner, None, params)
-
-def init_decoder(*, key: Array, model_config: ModelConfig) -> Decoder:
-    return stack_leaves([init_decoder_block(key=subkey, model_config=model_config) for subkey in rand.split(key, num=model_config.n_layers)])
 
 @partial(jax.jit, static_argnames=('model_config',))
 def forward_decoder(params: Decoder, seq: Array, qk_mask: Array, *, rotary_values: RotaryValues, kv_cache: KVCache | None=None, key: Array | None=None, model_config: ModelConfig) -> tuple[Array, KVCache | None]:
