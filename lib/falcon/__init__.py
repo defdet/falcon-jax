f854cd301909a57309a67ccdbc4a971d02f2f1b2
Max <bolgov1458@yandex.ru>
2024-02-05 08:11:49 +0300

    Update __init__.py

--- lib/falcon/__init__.py
+++ lib/falcon/__init__.py
@@ -2,6 +2,6 @@ from .ModelConfig import ModelConfig, model_config_falcon_7B
 from .kv_cache import KVCache, shift_left_kv_cache
 from .falcon import Falcon, forward_falcon
 from .falcon_model import FalconModel, forward_falcon_model
-from .attention import Attention, forward_attentiion
+from .attention import Attention, forward_attention
 from .layer_norm import LayerNorm, forward_layer_norm
 from .rotary_embedding import RotaryValues, get_rotary_values_at_position, make_rotary_values
